277f06fa1b8df0acf4c34244d7a7166a4a4ffb24
devhasanmia <dev.hasanmia@gmail.com>
2025-04-30 23:30:34 +0600

    added basic data types

---
+++ module1/src/index.ts
@@ -0,0 +1,22 @@
+// Basic Data Types
+let firstName: string = 'MD. HASAN MIA';
+// Number
+let roll: number = 123;
+// Boolean
+let isManager: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null
+// any type
+let d;
+
+// Non Primitive Data Types
+// Array
+
+let friends: string[] = ['HASAN', 'HRIDAY']
+let eligibleRollList: number[] = [1,2];
+
+eligibleRollList.push(111);
+
+
