ba05dce96613284146a45e9a76a29b1597903259
unknown <dev.hasanmia@gmail.com>
2025-05-01 14:09:08 +0600

    feat(types): define Student type alias with optional contact number

---
+++ module1/src/typeAlias.ts
@@ -0,0 +1,42 @@
+{
+
+    type Student = {
+        name: string,
+        age: number,
+        gender: "Male" | "Female",
+        address: string,
+        contactNo?: string
+    }
+
+
+
+const student1: Student = {
+    name: "SDf",
+    age: 50,
+    gender: "Female",
+    address: "ss"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
